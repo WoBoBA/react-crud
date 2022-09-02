4a09949e075cac5946fb3f93ceced29997885a30
WoBoBA <112345793+WoBoBA@users.noreply.github.com>
2022-09-03 01:36:48 +0700

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Link from "@mui/material/Link";
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Link href="/">
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} color="white">
+              CRUD
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/Users.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Avatar from "@mui/material/Avatar";
+import Link from "@mui/material/Link";
+import { ButtonGroup } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+export default function Users() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const UserGet = () => {
+    fetch("https://www.mecallapi.com/api/users")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  };
+
+  const UserUpdate = (id) => {
+    window.location = "/update/" + id;
+  };
+
+  const UserDelete = (id) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      id: id,
+    });
+
+    var requestOptions = {
+      method: "DELETE",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("https://www.mecallapi.com/api/users/delete", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        alert(result["message"]);
+        if (result["status"] === "ok") {
+          UserGet();
+        }
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    UserGet();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="lg" sx={{ p: 2 }}>
+          <Paper sx={{ p: 2 }}>
+            <Box display="flex" sx={{ p: 2 }}>
+              <Box sx={{ flexGrow: 1 }}>
+                <Typography variant="h6" gutterBottom component="div">
+                  Users
+                </Typography>
+              </Box>
+              <Box>
+                <Link href="/create">
+                  <Button variant="contained">Create</Button>
+                </Link>
+              </Box>
+            </Box>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell align="center">Avatar</TableCell>
+                    <TableCell align="center">First Name</TableCell>
+                    <TableCell align="center">Last Name</TableCell>
+                    <TableCell align="center">Username</TableCell>
+                    <TableCell align="center">Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {items.map((item) => (
+                    <TableRow
+                      key={item.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {item.id}
+                      </TableCell>
+                      <TableCell align="center">
+                        <Box display="flex" justifyContent="center">
+                          <Avatar alt={item.username} src={item.avatar} />
+                        </Box>
+                      </TableCell>
+                      <TableCell align="center">{item.fname}</TableCell>
+                      <TableCell align="center">{item.lname}</TableCell>
+                      <TableCell align="center">{item.username}</TableCell>
+                      <TableCell align="right">
+                        <ButtonGroup
+                          variant="outlined"
+                          aria-label="outlined button group"
+                        >
+                          <Button onClick={() => UserUpdate(item.id)} startIcon={<EditIcon />}>
+                            Edit
+                          </Button>
+                          <Button onClick={() => UserDelete(item.id)} startIcon={<DeleteIcon />}>
+                            Delete
+                          </Button>
+                        </ButtonGroup>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Container>
+      </React.Fragment>
+    );
+  }
+}
